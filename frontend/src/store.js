bbf0b9a9bf1680fa64078728a3079a79c7dbd286
Sumit Kumar <112721086+hySumit@users.noreply.github.com>
2024-07-21 19:28:08 +0530

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import transactionReducer from './components/State_Manager/transactionReducer';
-// Create the Redux store with devtools support (built into configureStore)
 const store = configureStore({
   reducer: {
     transactions: transactionReducer,
   },
-  devTools: process.env.NODE_ENV !== 'production', // Enables Redux DevTools Extension
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
